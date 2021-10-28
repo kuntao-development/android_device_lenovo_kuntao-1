bc69c4267e0f77ce88275864b2bf5781fc30ba2a
Murat Kozan <muratkozan350@gmail.com>
2021-10-28 22:31:50 +0300

    kuntao: Fix power hal on aex

--- device.mk
+++ device.mk
@@ -307,7 +307,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
