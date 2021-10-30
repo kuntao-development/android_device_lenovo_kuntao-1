61807233f63de4f5fcd5802e29939ea4e1c9f67c
Murat Kozan <muratkozan350@gmail.com>
2021-10-30 11:07:13 +0300

    kuntao: Set product qcom hal

--- device.mk
+++ device.mk
@@ -31,6 +31,10 @@ PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Hal
+PRODUCT_USES_QCOM_HARDWARE := true
+PRODUCT_BOARD_PLATFORM := msm8996
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
