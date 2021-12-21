18a9e756240261d36682a8d583808365ec92dc55
Murat Kozan <muratkozan350@gmail.com>
2021-12-21 22:26:50 +0300

    Revert "Revert "kuntao: Drop lmkd support""

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -193,25 +193,8 @@ echo 200000 > /proc/sys/kernel/sched_freq_dec_notify
 #
 # Read adj series and set adj threshold for PPR and ALMK.
 # This is required since adj values change from framework to framework.
-adj_series=`cat /sys/module/lowmemorykiller/parameters/adj`
-adj_1="${adj_series#*,}"
-set_almk_ppr_adj="${adj_1%%,*}"
-# PPR and ALMK should not act on HOME adj and below.
-# Normalized ADJ for HOME is 6. Hence multiply by 6
-# ADJ score represented as INT in LMK params, actual score can be in decimal
-# Hence add 6 considering a worst case of 0.9 conversion to INT (0.9*6).
-set_almk_ppr_adj=$(((set_almk_ppr_adj * 6) + 6))
-echo $set_almk_ppr_adj > /sys/module/lowmemorykiller/parameters/adj_max_shift
-echo $set_almk_ppr_adj > /sys/module/process_reclaim/parameters/min_score_adj
 echo 0 > /sys/module/process_reclaim/parameters/enable_process_reclaim
-echo 70 > /sys/module/process_reclaim/parameters/pressure_max
-echo 30 > /sys/module/process_reclaim/parameters/swap_opt_eff
 echo 0 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
-echo 10 > /sys/module/process_reclaim/parameters/pressure_min
-echo 1024 > /sys/module/process_reclaim/parameters/per_swap_size
-echo "18432,23040,27648,32256,55296,80640" > /sys/module/lowmemorykiller/parameters/minfree
-# For 64-bit arch, vmpressure_file_min = LMK minfree's last bin value
-echo 80640 > /sys/module/lowmemorykiller/parameters/vmpressure_file_min
 
 # Set 512 for > 3GB
 echo 512 > /sys/block/mmcblk0/bdi/read_ahead_kb
