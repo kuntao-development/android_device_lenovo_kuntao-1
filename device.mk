3a59a6965520e114ad5299f3a3e1a89d45083a37
Murat Kozan <muratkozan350@gmail.com>
2022-03-17 02:41:31 +0300

    kuntao: Restore to Health 2.0 HAL

--- device.mk
+++ device.mk
@@ -167,9 +167,7 @@ PRODUCT_COPY_FILES += \
 
 # Healthd
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl:64 \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
+    android.hardware.health@2.0-service
 
 # HIDL
 PRODUCT_PACKAGES += \
