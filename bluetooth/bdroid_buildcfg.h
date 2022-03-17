0a0c7ded1f5478263733178c9a01973f4402e1df
Murat Kozan <muratkozan350@gmail.com>
2022-03-17 04:37:55 +0300

    kuntao: Set bt name to p2a42

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME             "Lenovo P2"
+#define BTM_DEF_LOCAL_NAME             "Lenovo P2a42"
 
 #define BLE_VND_INCLUDED               TRUE
 #define BTM_SCO_ENHANCED_SYNC_ENABLED  FALSE
