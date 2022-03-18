12a87703ab8f7a5c034b7bb0e08828a1c66be9eb
Murat Kozan <muratkozan350@gmail.com>
2022-03-19 01:12:17 +0300

    kuntao: Disable sdm rotator downscaler
    Rotator down scaler is causing additional delays that are better
    handled by GL composition.
    
    Test: Open app, no jank

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -70,6 +70,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196610 \
     ro.vendor.display.cabl=0 \
     vendor.display.disable_skip_validate=1 \
+    vendor.display.disable_rotator_downscale=1 \
     vendor.display.enable_default_color_mode=1 \
     vendor.gralloc.enable_fb_ubwc=1
 
