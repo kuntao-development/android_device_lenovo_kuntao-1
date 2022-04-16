33ce8b4459d4e2637879fbd6a88b78591ad3a2a5
Murat Kozan <muratkozan350@gmail.com>
2022-04-16 20:19:14 +0300

    kuntao: Set telephony vt cam interface to 2

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -109,7 +109,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # IMS
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.volte_avail_ovr=1 \
-    persist.dbg.vt_avail_ovr=1
+    persist.dbg.vt_avail_ovr=1 \
+    persist.vendor.qti.telephony.vt_cam_interface=2
 
 # LMKD
 PRODUCT_PROPERTY_OVERRIDES += \
