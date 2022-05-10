4467a9c321842603c3f5194cef6da09f5b8aa194
Murat Kozan <muratkozan350@gmail.com>
2022-05-10 17:09:52 +0300

    kuntao: Enable ANT+ support

--- device.mk
+++ device.mk
@@ -56,6 +56,11 @@ PRODUCT_ENFORCE_RRO_TARGETS += *
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
+# ANT+
+PRODUCT_PACKAGES += \
+    AntHalService \
+    libantradio
+
 # Audio
 PRODUCT_PACKAGES += \
     audio_amplifier.msm8953 \
