8fd20a301cc80b8e8679719e90f4f88f19119c05
Murat Kozan <muratkozan350@gmail.com>
2022-05-12 16:57:59 +0300

    kuntao: Move log.tag props to system

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -41,10 +41,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.qcom.bluetooth.soc=smd
 
-# Charger
-PRODUCT_PROPERTY_OVERRIDES += \
-    log.tag.smart_charger=W
-
 # CNE
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.cne.feature=1
@@ -78,9 +74,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Fingerprint
 PRODUCT_PROPERTY_OVERRIDES += \
-    log.tag.synaFpHal=W \
-    log.tag.SynapticsLIB=W \
-    log.tag.synaTransport=W \
     persist.vendor.fp.gesture=1 \
     persist.sys.fp.fingerUpCheck=0 \
     persist.sys.fp.tplUpdYieldTime=400 \
