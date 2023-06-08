5d85a84afab27df0fd32ae9e16a7b78ac15bdead
Astridxx <muratkozan350@gmail.com>
2023-06-08 23:50:48 +0300

    kuntao: vendorsetup.sh Use depth=1

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,11 +2,11 @@ echo 'Starting to clone stuffs needed for your device'
 
 echo 'Cloning kernel tree [1/15]'
 # Kernel
-rm -rf kernel/lenovo/msm8953 && git clone https://github.com/kuntao-development/android_kernel_lenovo_msm8953 -b lineage-19.1 kernel/lenovo/msm8953 
+rm -rf kernel/lenovo/msm8953 && git clone --depth=1 https://github.com/kuntao-development/android_kernel_lenovo_msm8953 -b lineage-19.1 kernel/lenovo/msm8953 
 
 echo 'Cloning vendor tree [2/15]'
 # Vendor
-rm -rf vendor/lenovo && git clone https://github.com/Astridxx/proprietary_vendor_lenovo -b los-19.1 vendor/lenovo
+rm -rf vendor/lenovo && git clone --depth=1 https://github.com/Astridxx/proprietary_vendor_lenovo -b los-19.1 vendor/lenovo
 
 echo 'Cloning frameworks base [3/15]'
 # Frameworks
@@ -14,43 +14,43 @@ rm -rf frameworks/base && git clone --depth=1 https://github.com/Astridxx/androi
 
 echo 'Cloning system netd [4/15]'
 # Netd
-rm -rf system/netd && git clone https://github.com/Astridxx/android_system_netd -b lineage-19.1 system/netd
+rm -rf system/netd && git clone --depth=1 https://github.com/Astridxx/android_system_netd -b lineage-19.1 system/netd
 
 echo 'Cloning system bpf [5/15]'
 # Bpf
-rm -rf system/bpf && git clone https://github.com/Astridxx/android_system_bpf -b lineage-19.1 system/bpf
+rm -rf system/bpf && git clone --depth=1 https://github.com/Astridxx/android_system_bpf -b lineage-19.1 system/bpf
 
 echo 'Cloning system libhidl [6/15]'
 # Hidl
-rm -rf system/libhidl && git clone https://github.com/Astridxx/android_system_libhidl -b lineage-19.1 system/libhidl
+rm -rf system/libhidl && git clone --depth=1 https://github.com/Astridxx/android_system_libhidl -b lineage-19.1 system/libhidl
 
 echo 'Cloning system sepolicy [7/15]'
 # Sepolicy
-rm -rf system/sepolicy && git clone https://github.com/LineageOS-UL/android_system_sepolicy -b lineage-19.1 system/sepolicy
+rm -rf system/sepolicy && git clone --depth=1 https://github.com/LineageOS-UL/android_system_sepolicy -b lineage-19.1 system/sepolicy
 
 echo 'Cloning system core [8/15]'
 # Core
-rm -rf system/core && git clone https://github.com/Astridxx/android_system_core -b lineage-19.1 system/core
+rm -rf system/core && git clone --depth=1 https://github.com/Astridxx/android_system_core -b lineage-19.1 system/core
 
 echo 'Cloning frameworks native [9/15]'
 # Native
-rm -rf frameworks/native && git clone https://github.com/Astridxx/android_frameworks_native-2 -b lineage-19.1 frameworks/native
+rm -rf frameworks/native && git clone --depth=1 https://github.com/Astridxx/android_frameworks_native-2 -b lineage-19.1 frameworks/native
 
 echo 'Cloning system logging [10/15]'
 # Logging
-rm -rf system/logging && git clone https://github.com/Astridxx/android_system_logging -b lineage-19.1 system/logging
+rm -rf system/logging && git clone --depth=1 https://github.com/Astridxx/android_system_logging -b lineage-19.1 system/logging
 
 echo 'Cloning deskclock [11/15]'
 # DeskClock
-rm -rf packages/apps/DeskClock && git clone https://github.com/Astridxx/android_packages_apps_DeskClock-1 -b lineage-19.1 packages/apps/DeskClock
+rm -rf packages/apps/DeskClock && git clone --depth=1 https://github.com/Astridxx/android_packages_apps_DeskClock-1 -b lineage-19.1 packages/apps/DeskClock
 
 echo 'Cloning settings [12/15]'
 # Settings
-rm -rf packages/apps/Settings && git clone https://github.com/Astridxx/android_packages_apps_Settings -b lineage-19.1 packages/apps/Settings
+rm -rf packages/apps/Settings && git clone --depth=1 https://github.com/Astridxx/android_packages_apps_Settings -b lineage-19.1 packages/apps/Settings
 
 echo 'Cloning revamped fmradio [13/15]'
 # Fm radio
-git clone https://github.com/iusmac/RevampedFMRadio -b qcom packages/apps/RevampedFMRadio
+git clone --depth=1 https://github.com/iusmac/RevampedFMRadio -b qcom packages/apps/RevampedFMRadio
 
 echo 'Adding patch for Trebuchet [14/15]'
 cd packages/apps/Trebuchet
